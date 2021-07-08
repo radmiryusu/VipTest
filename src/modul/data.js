0895c2e582f3267a36a81c881688263c9a2fd07a
radmir <radmiryusupov1999@mail.ru>
2021-07-08 18:57:46 +0300

    ref project

--- src/modul/data.js
+++ src/modul/data.js
@@ -1,4 +1,10 @@
-module.exports=  async function FetchMethods(url) {
-  const response = await fetch(url);
+module.exports = async function FetchMethods(url) {
+  const headers = {
+    Authorization: "token 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",
+  };
+  const response = await fetch(url, {
+    method: "GET",
+    headers:headers
+  });
   return response.ok ? await response.json() : "";
 };
--- src/store/index.js
+++ src/store/index.js
@@ -17,20 +17,19 @@ export default new Vuex.Store({
   actions: {
     //получение профился пользователя
     dataProfile({ commit, state }) {
-      const url = `https://api.github.com/users/${state.person}`;
-      FetchMethods(url)
+      FetchMethods(`https://api.github.com/users/${state.person}`)
         .then((res) => {
-          console.log(res);
           commit("ListProfile", res);
           return res;
         })
         .then(({ repos_url, following_url }) => {
-          Promise.allSettled([
+          const link = Promise.allSettled([
             FetchMethods(repos_url),
             FetchMethods(following_url.split(/{\/[a-z]*\D[a-z]*}/)[0]),
-          ]).then((res) => {
-            commit("ListReposit", res[0].value);
-            commit("ListSubscribers", res[1].value);
+          ]);
+          link.then(([repost,subscr]) => {
+            commit("ListReposit", repost.value);
+            commit("ListSubscribers", subscr.value);
           });
         });
     },
@@ -39,12 +38,9 @@ export default new Vuex.Store({
       if (data.length !== 0) {
         commit("AllPersons", data);
       } else {
-        axios
-          .get("https://api.github.com/users?since=50000000", {
-            Authorization: "token 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",
-          })
+        FetchMethods("https://api.github.com/users?since=50000000")
           .then((resolve) => {
-            commit("AllPersons", resolve.data);
+            commit("AllPersons", resolve);
           })
           .catch((e) => {
             console.log(e);
